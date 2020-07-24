23499695e60de0eb19416e67e2cf397c59558ecd
Glen Daniels <glen.daniels@accenture.com>
2020-07-24 14:03:19 -0400

    Update Offers, ReceiptPrinter, etc

--- app/src/main/java/supermarket/model/ShoppingCart.kt
+++ app/src/main/java/supermarket/model/ShoppingCart.kt
@@ -1,26 +1,15 @@
 package supermarket.model
 
 import java.util.ArrayList
-import java.util.HashMap
 
 class ShoppingCart {
 
-    private val items = ArrayList<ProductQuantity>()
-    internal var productQuantities: MutableMap<Product, Double> = HashMap()
-
+    private val items = mutableListOf<ProductQuantity>()
+    internal val productQuantities = mutableMapOf<Product, Double>()
 
     internal fun getItems(): List<ProductQuantity> {
         return ArrayList(items)
     }
-
-    internal fun addItem(product: Product) {
-        this.addItemQuantity(product, 1.0)
-    }
-
-    internal fun productQuantities(): Map<Product, Double> {
-        return productQuantities
-    }
-
 
     fun addItemQuantity(product: Product, quantity: Double) {
         items.add(ProductQuantity(product, quantity))
@@ -31,49 +20,15 @@ class ShoppingCart {
         }
     }
 
-    internal fun handleOffers(receipt: Receipt, offers: Map<Product, Offer>, catalog: SupermarketCatalog) {
-        for (p in productQuantities().keys) {
-            val quantity = productQuantities[p]!!
-            if (offers.containsKey(p)) {
-                val offer = offers[p]!!
-                val unitPrice = catalog.getUnitPrice(p)
-                val quantityAsInt = quantity.toInt()
-                var discount: Discount? = null
-                var x = 1
-                if (offer.offerType === SpecialOfferType.ThreeForTwo) {
-                    x = 3
-
-                } else if (offer.offerType === SpecialOfferType.TwoForAmount) {
-                    x = 2
-                    if (quantityAsInt >= 2) {
-                        val total = offer.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice
-                        val discountN = unitPrice * quantity - total
-                        discount = Discount(p, "2 for " + offer.argument, discountN)
-                    }
-
-                }
-                if (offer.offerType === SpecialOfferType.FiveForAmount) {
-                    x = 5
-                }
-                val numberOfXs = quantityAsInt / x
-                if (offer.offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    val discountAmount =
-                        quantity * unitPrice - (numberOfXs.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
-                    discount = Discount(p, "3 for 2", discountAmount)
-                }
-                if (offer.offerType === SpecialOfferType.TenPercentDiscount) {
-                    discount =
-                        Discount(p, offer.argument.toString() + "% off", quantity * unitPrice * offer.argument / 100.0)
-                }
-                if (offer.offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    val discountTotal =
-                        unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice)
-                    discount = Discount(p, x.toString() + " for " + offer.argument, discountTotal)
-                }
-                if (discount != null)
-                    receipt.addDiscount(discount)
+    internal fun handleOffers(receipt: Receipt, offers: List<Offer>, catalog: SupermarketCatalog) {
+        offers.forEach { offer ->
+            offer.getDiscount(this, catalog)?.let {
+                receipt.addDiscount(it)
             }
-
         }
+    }
+
+    fun quantityOf(product: Product): Double? {
+        return productQuantities[product]
     }
 }
--- app/src/main/java/supermarket/model/Teller.kt
+++ app/src/main/java/supermarket/model/Teller.kt
@@ -3,10 +3,10 @@ package supermarket.model
 import java.util.HashMap
 
 class Teller(private val catalog: SupermarketCatalog) {
-    private val offers = HashMap<Product, Offer>()
+    private val offers = mutableListOf<Offer>()
 
-    fun addSpecialOffer(offerType: SpecialOfferType, product: Product, argument: Double) {
-        this.offers[product] = Offer(offerType, product, argument)
+    fun addSpecialOffer(offer: Offer) {
+        offers.add(offer)
     }
 
     fun checksOutArticlesFrom(theCart: ShoppingCart): Receipt {
@@ -15,11 +15,11 @@ class Teller(private val catalog: SupermarketCatalog) {
         for (pq in productQuantities) {
             val p = pq.product
             val quantity = pq.quantity
-            val unitPrice = this.catalog.getUnitPrice(p)
+            val unitPrice = catalog.getUnitPrice(p)
             val price = quantity * unitPrice
             receipt.addProduct(p, quantity, unitPrice, price)
         }
-        theCart.handleOffers(receipt, this.offers, this.catalog)
+        theCart.handleOffers(receipt, offers, catalog)
 
         return receipt
     }
